f2b0c5cc3ed1bec8ef5ef5d21e2af03dc91f5806
Stuart Shelton <stuart.shelton@hp.com>
2014-04-03 15:31:28 +0100

    Re-add preseeding logic - apparently MariaDB pretends to be mysql-server...

--- recipes/_server_debian.rb
+++ recipes/_server_debian.rb
@@ -1,27 +1,27 @@
+#----
+# Set up preseeding data for debian packages
+#---
+directory '/var/cache/local/preseeding' do
+  owner     'root'
+  group     'root'
+  mode      '0755'
+  recursive  true
+end
+
+template '/var/cache/local/preseeding/mysql-server.seed' do
+  source   'mysql-server.seed.erb'
+  owner    'root'
+  group    'root'
+  mode     '0600'
+  notifies :run, 'execute[preseed mysql-server]', :immediately
+end
+
+execute 'preseed mysql-server' do
+  command '/usr/bin/debconf-set-selections /var/cache/local/preseeding/mysql-server.seed'
+  action  :nothing
+end
+
 if node['mysql']['implementation'] != 'mariadb' && node['mysql']['implementation'] != 'galera'
-
-  #----
-  # Set up preseeding data for debian packages
-  #---
-  directory '/var/cache/local/preseeding' do
-    owner     'root'
-    group     'root'
-    mode      '0755'
-    recursive  true
-  end
-
-  template '/var/cache/local/preseeding/mysql-server.seed' do
-    source   'mysql-server.seed.erb'
-    owner    'root'
-    group    'root'
-    mode     '0600'
-    notifies :run, 'execute[preseed mysql-server]', :immediately
-  end
-
-  execute 'preseed mysql-server' do
-    command '/usr/bin/debconf-set-selections /var/cache/local/preseeding/mysql-server.seed'
-    action  :nothing
-  end
 
   node['mysql']['server']['packages'].each do |name|
     package name do
