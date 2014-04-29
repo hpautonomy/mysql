58589abe12d30658a22db46cf998c16be5823310
Stuart Shelton <stuart.shelton@hp.com>
2014-04-29 10:38:50 +0100

    Revert to a hard requirement of build-essential-v1.4.4, as v2.0.0 is causing issues with other cookbooks

--- recipes/ruby.rb
+++ recipes/ruby.rb
@@ -20,7 +20,11 @@
 # limitations under the License.
 #
 
-node.set['build-essential']['compile_time'] = true
+# build-essential-v2.0.0+:
+#node.set['build-essential']['compile_time'] = true
+# build-essential-v1.4.4:
+node.set['build-essential']['compiletime'] = true
+
 include_recipe 'build-essential::default'
 include_recipe 'mysql::client'
 
